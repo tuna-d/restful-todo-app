aed647a4ab6c05c1a9f5ea0bac3058ef739a9c00
tuna-d <tdemirel98@gmail.com>
2024-11-04 23:31:06 +0300

    add index and show functions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# restful-todo-app
- 
+# Todo App
+
+This is a simple Todo application built with Express to practice RESTful routes. The app allows users to create, read, update, and delete (CRUD) todos, following REST principles.
+
+## Features
+
+- **Create** a new todo
+- **Read** existing todos
+- **Update** details of an existing todo
+- **Delete** a todo
+
+## Technologies Used
+
+- **Express**: Web framework for Node.js
+- **EJS**: Embedded JavaScript templates for server-side rendering
+- **Tailwind CSS** (via CDN): Utility-first CSS framework for styling
+- **Body-parser**: Middleware to parse incoming request bodies
+- **Nodemon** (for development): Automatically restarts the server on file changes
+- **uuid**: Used for creating unique id's to todos
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 const path = require("path")
+const { v4: uuidv4 } = require("uuid")
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
@@ -10,6 +11,27 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
 
+let todos = [
+  {
+    id: uuidv4(),
+    task: "shopping",
+    due: "friday",
+    importance: "normal",
+  },
+  {
+    id: uuidv4(),
+    task: "car wash",
+    due: "monday",
+    importance: "urgent",
+  },
+]
+
 app.get("/", (req, res) => {
-  res.render("home")
+  res.render("home", { todos })
+})
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params
+  const findTodo = todos.find((t) => t.id == id)
+  res.render("show", { findTodo })
 })
